00e15acb6a53cbed869c2db283c0de888b899b16
sindura.venkatesh <sindura.venkatesh@gmail.com>
2019-05-31 13:28:50 +0530

    Commiting restaurant changes

--- RestaurantSite/RestaurantSite/Controllers/HomeController.cs
+++ RestaurantSite/RestaurantSite/Controllers/HomeController.cs
@@ -4,97 +4,84 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RestaurantSite.Models;
+
 namespace RestaurantSite.Controllers
 {
     
     public class HomeController : Controller
     {
-        List<RestaurantData> restlist;
+        masterEntities dbobj = new masterEntities();
+        List<tabledetail> tablelist;
         public ActionResult Index()
         {
-            restlist = new List<RestaurantData>();
-            restlist.Add(new RestaurantData(100, 10, false));
-            restlist.Add(new RestaurantData(101, 10, false));
-            restlist.Add(new RestaurantData(102, 6, false));
-            restlist.Add(new RestaurantData(103, 6, false));
-            restlist.Add(new RestaurantData(104, 6, false));
-            restlist.Add(new RestaurantData(105, 4, false));
-            restlist.Add(new RestaurantData(106, 4, false));
-            restlist.Add(new RestaurantData(107, 2, false));
-            restlist.Add(new RestaurantData(108, 2, false));
-            restlist.Add(new RestaurantData(109, 2, false));
-
-            return View(restlist);
+            
+            return View(dbobj.tabledetails.ToList());
         }
 
-        public ActionResult GetDataFromCustomer(int  numberofpersons=12)
+        public ActionResult GetDataFromCustomer(int  numberofpersons)
 
         {
             if(areTablesAvailable(numberofpersons))
             {
                 AllocateSeats(numberofpersons);
-                
+                return RedirectToAction("Index");
+
             }
             else
             {
                 return Content("Tables are not vacant now. Please try after some time.");
+               
             }
 
 
-            return View();
+           
         }
         public bool areTablesAvailable(int noofppl)
         {
-            bool vacancyavailable = false;
+            tablelist = dbobj.tabledetails.ToList();
+            
             int count=0;
-            foreach(var table in restlist)
+            foreach(var table in tablelist)
             {
-                if(table.isoccupied==false)
+                if(table.istableoccupies==false)
                 {
-                    count = count + table.table_capacity;
+                    count = count + table.capaciyoftable;
                 }
             }
-            if (count > noofppl)
+            if (count >= noofppl)
             {
-                vacancyavailable = true;
-            }
-            return vacancyavailable;
-        }
-        public void AllocateSeats(int noofppl)
-        {
-            int totalppl = noofppl;
-            if (totalppl == 1)
-            {
-                foreach (var table in restlist)
-                {
-                    if (table.table_capacity == 2)
-                    {
-                        table.isoccupied = true;
-                    }
-                }
+                return true;
             }
             else
             {
-                while (totalppl > 0)
-                {
-                    foreach (var table in restlist)
+                return false;
+            }
+        }
+        public void AllocateSeats(int noofppl)
+         {
+           
+            int totalppl = noofppl;
+            
+                    foreach (var table in tablelist)
                     {
-                        if (table.isoccupied == false)
+                        if (table.istableoccupies == false && totalppl / table.capaciyoftable >= 1)
                         {
-                            if (totalppl / table.table_capacity >= 1)
-                            {
-                                table.isoccupied = true;
-                                totalppl = totalppl - table.table_capacity;
-                                break;
-
-                            }
+                            dbobj.sp_updateoccupancystatus(table.table_number);
+                            totalppl = totalppl - table.capaciyoftable;
+                           
 
                         }
 
-                    }
-                }
-            }
-            
-        }
-    }
-}
+                        else if(totalppl==1)
+                            {foreach (var table1 in tablelist.AsEnumerable().Reverse())
+                                {if (table1.istableoccupies == false)
+                        {
+                            dbobj.sp_updateoccupancystatus(table1.table_number);
+                            totalppl = totalppl - table.capaciyoftable;
+                            break;
+                        }
+                            } }}
+                    
+
+                    } }}
+
